1e320eda75ed96a5f2722b9007f2817a1185abf7
LukasSkibidi <lukas.sokic@htl-wels.at>
2026-06-02 10:39:33 +0200

    Schere stein papier Model fertig

--- src/model/SchereSteinPapier/STPModel.java
+++ src/model/SchereSteinPapier/STPModel.java
@@ -1,4 +1,38 @@
 package model.SchereSteinPapier;
 
+import java.util.Random;
+
 public class STPModel {
+    Random rd = new Random();
+
+    public void ergebnis(int userinput){
+        //1 = Schere
+        //2 = Stein
+        //3 = Papier
+        int r = rd.nextInt(3)+1;
+
+        if (userinput == r){
+            System.out.println("Unentschieden!");
+        } else if (userinput == 1) {
+            if (r == 2){
+                System.out.println("Du hast Verloren?");
+            } else if (r == 3) {
+                System.out.println("Du hast Gewonnen!");
+            }
+
+        }else if (userinput == 2) {
+            if (r == 1){
+                System.out.println("Du hast Gewonnen!");
+            }else if (r == 3) {
+                System.out.println("Du hast Verloren?");
+
+        }
+        }else if (userinput == 3) {
+            if (r == 1){
+                System.out.println("Du hast Verloren?");
+            }else if (r == 2) {
+                System.out.println("Du hast Verloren?");
+            }
+        }
+    }
 }
