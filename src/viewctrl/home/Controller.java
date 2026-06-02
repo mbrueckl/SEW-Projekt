055146c3cb97cde59ceace0d24122067b3ab095a
Larissa Mair <larissa.mair@htl-wels.at>
2026-06-02 10:08:10 +0200

    zwischen commit 1

--- src/viewctrl/home/Controller.java
+++ src/viewctrl/home/Controller.java
@@ -1,4 +1,28 @@
-package viewctrl.home;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
-public class Controller {
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField pwTF;
+
+    @FXML
+    private TextField userTF;
+
+    @FXML
+    void anmelden(ActionEvent event) {
+
+    }
+
+    @FXML
+    void benutzerErstellen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
 }
