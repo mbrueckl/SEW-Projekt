e45724df44f20aa5f448ef50155373ac7fb2d1c5
LukasSkibidi <lukas.sokic@htl-wels.at>
2026-06-26 11:46:14 +0200

    4 Gewinnt logik

--- src/model/vierGewinnt/VierGewinntModel.java
+++ src/model/vierGewinnt/VierGewinntModel.java
@@ -1,40 +1,123 @@
 package model.vierGewinnt;
 
 public class VierGewinntModel {
+
     int[][] feld = new int[6][7];
+
     int globalcounter;
+    int aktuellerSpieler = 1;
+
     /*
-[0][0] [0][1] [0][2] [0][3] [0][4] [0][5] [0][6]
-[1][0] [1][1] [1][2] [1][3] [1][4] [1][5] [1][6]
-[2][0] [2][1] [2][2] [2][3] [2][4] [2][5] [2][6]
-[3][0] [3][1] [3][2] [3][3] [3][4] [3][5] [3][6]
-[4][0] [4][1] [4][2] [4][3] [4][4] [4][5] [4][6]
-[5][0] [5][1] [5][2] [5][3] [5][4] [5][5] [5][6]
+    0 = leer
+    1 = Spieler 1
+    2 = Spieler 2
     */
 
-    public void spielen(int counter, int userRow){
-        //WICHTIG: Counter im Controller auf 5 und UserRow user input usen
+    public void spielen(int counter, int userRow) {
 
-        //leeres Feld = 0
-        //Player 1 = 1
-        //Player 2 = 2
-        if (feld[0][userRow - 1] != 0){
-            System.out.println("Diese Row ist voll");
-            globalcounter--;
+        if (feld[0][userRow - 1] != 0) {
+            System.out.println("Diese Spalte ist voll");
+            return;
         }
-        else if (feld[counter][userRow - 1] == 0){
-            if (globalcounter % 2 == 0){
-                feld[counter][userRow - 1] = 1;
-            } else if (globalcounter % 2 > 0) {
-                feld[counter][userRow - 1] = 2;
-            }else {
-                spielen(counter - 1, userRow);
+
+        if (feld[counter][userRow - 1] == 0) {
+
+            feld[counter][userRow - 1] = aktuellerSpieler;
+            globalcounter++;
+
+            if (vertikal(aktuellerSpieler)
+                    || horizontal(aktuellerSpieler)
+                    || diagonal(aktuellerSpieler)) {
+
+                System.out.println("Spieler "
+                        + aktuellerSpieler
+                        + " gewinnt!");
             }
-        }
-        globalcounter++;
-        if (globalcounter >= 8){
+            else {
+                switchPlayer();
+            }
 
+            return;
         }
+
+        spielen(counter - 1, userRow);
     }
 
+
+    public boolean vertikal(int spieler) {
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 7; col++) {
+
+                if (feld[row][col] == spieler
+                        && feld[row + 1][col] == spieler
+                        && feld[row + 2][col] == spieler
+                        && feld[row + 3][col] == spieler) {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public boolean horizontal(int spieler) {
+
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 4; col++) {
+
+                if (feld[row][col] == spieler
+                        && feld[row][col + 1] == spieler
+                        && feld[row][col + 2] == spieler
+                        && feld[row][col + 3] == spieler) {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public boolean diagonal(int spieler) {
+
+        // links oben -> rechts unten
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 4; col++) {
+
+                if (feld[row][col] == spieler
+                        && feld[row + 1][col + 1] == spieler
+                        && feld[row + 2][col + 2] == spieler
+                        && feld[row + 3][col + 3] == spieler) {
+
+                    return true;
+                }
+            }
+        }
+        // links unten -> rechts oben
+        for (int row = 3; row < 6; row++) {
+            for (int col = 0; col < 4; col++) {
+
+                if (feld[row][col] == spieler
+                        && feld[row - 1][col + 1] == spieler
+                        && feld[row - 2][col + 2] == spieler
+                        && feld[row - 3][col + 3] == spieler) {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public void switchPlayer() {
+
+        if (aktuellerSpieler == 1) {
+            aktuellerSpieler = 2;
+        } else {
+            aktuellerSpieler = 1;
+        }
+
+    }
 }
